19c590a1240ed0cae23038f7e8fdfe9ec00be8b3
KamilBlanik <235629@student.pwr.edu.pl>
2018-05-01 16:40:39 +0200

    .

--- PO_RPG_v2/windowManager.cpp
+++ PO_RPG_v2/windowManager.cpp
@@ -1,12 +1,14 @@
 #include "windowManager.h"
-
+#include "Player.h"
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
 void main(array<String^>^ args) {
+	// inicjalizacja okienka
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	PORPGv2::windowManager form;
 	Application::Run(%form);
+	//
 }
--- PO_RPG_v2/windowManager.h
+++ PO_RPG_v2/windowManager.h
@@ -34,6 +34,18 @@ namespace PORPGv2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  newGameButton;
+	protected:
+
+	private: System::Windows::Forms::Button^  loadButton;
+
+	private: System::Windows::Forms::Button^  exitButton;
+
+
+
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +60,77 @@ namespace PORPGv2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"windowManager";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(windowManager::typeid));
+			this->newGameButton = (gcnew System::Windows::Forms::Button());
+			this->loadButton = (gcnew System::Windows::Forms::Button());
+			this->exitButton = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// newGameButton
+			// 
+			this->newGameButton->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->newGameButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->newGameButton->Location = System::Drawing::Point(400, 250);
+			this->newGameButton->Name = L"newGameButton";
+			this->newGameButton->Size = System::Drawing::Size(200, 75);
+			this->newGameButton->TabIndex = 0;
+			this->newGameButton->Text = L"Rozpocznij now¹ grê";
+			this->newGameButton->UseVisualStyleBackColor = true;
+			this->newGameButton->Click += gcnew System::EventHandler(this, &windowManager::button1_Click);
+			// 
+			// loadButton
+			// 
+			this->loadButton->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->loadButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->loadButton->Location = System::Drawing::Point(400, 350);
+			this->loadButton->Name = L"loadButton";
+			this->loadButton->Size = System::Drawing::Size(200, 75);
+			this->loadButton->TabIndex = 1;
+			this->loadButton->Text = L"Wczytaj grê";
+			this->loadButton->UseVisualStyleBackColor = true;
+			this->loadButton->Click += gcnew System::EventHandler(this, &windowManager::button2_Click);
+			// 
+			// exitButton
+			// 
+			this->exitButton->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->exitButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->exitButton->Location = System::Drawing::Point(400, 450);
+			this->exitButton->Name = L"exitButton";
+			this->exitButton->Size = System::Drawing::Size(200, 75);
+			this->exitButton->TabIndex = 2;
+			this->exitButton->Text = L"Zakoñcz";
+			this->exitButton->UseVisualStyleBackColor = true;
+			this->exitButton->Click += gcnew System::EventHandler(this, &windowManager::button3_Click);
+			// 
+			// windowManager
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(982, 753);
+			this->Controls->Add(this->exitButton);
+			this->Controls->Add(this->loadButton);
+			this->Controls->Add(this->newGameButton);
+			this->Name = L"windowManager";
+			this->ShowIcon = false;
+			this->Text = L"DungeonExplorer";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+}
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+};
 }
